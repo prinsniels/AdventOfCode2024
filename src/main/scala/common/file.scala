3ac772d07ae8bc2cce74dfd17ea86981c9742906
Niels Prins <niels.prins@gmail.com>
2024-11-29 16:09:28 +0100

    init

---
+++ src/main/scala/common/file.scala
@@ -0,0 +1,16 @@
+package aoc
+package utils
+
+import scala.io.Source.fromFile
+import java.io.File
+import util.chaining.*
+
+def readFile(path: String): String =
+  val buffer = fromFile(new File(path))
+  val lines = buffer.getLines().mkString("\n")
+  buffer.close()
+  lines
+
+extension (a: String)
+  def sample: String = readFile(s"src/main/resources/sample/$a")
+  def live: String = readFile(s"src/main/resources/live/$a")
---
+++ src/main/scala/day01.scala
@@ -0,0 +1,20 @@
+package aoc
+import utils.{sample, live}
+
+import scala.util.chaining.*
+
+
+
+def part1(input: String): String =
+  ???
+
+def part2(input: String): String =
+  ???
+
+
+@main def part1: Unit =
+  println(s"The solution is ${part1("day01.txt".live)}")
+  
+@main def part2: Unit =
+  println(s"The solution is ${part2("day01.txt".live)}")
+
